d2cebb09c3d351a76aff745cef66fe47bf5fa7f4
skiesel <skiesel@cs.unh.edu>
2015-06-29 11:33:38 -0400

    headless vrep

--- start-vrep.sh
+++ start-vrep.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" != "" ];
 then
-	../dependencies/V-REP_PRO_EDU_V3_2_1_Mac/vrep.app/Contents/MacOS/vrep -g$1
+	../dependencies/V-REP_PRO_EDU_V3_2_1_Mac/vrep.app/Contents/MacOS/vrep -h -g$1
 else
 	echo "missing instance file parameter"
 fi
