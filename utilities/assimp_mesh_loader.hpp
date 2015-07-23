e6392dc91a7b9ed80bbd345f00e4bf708d3e1784
skiesel <skiesel@cs.unh.edu>
2015-07-23 12:58:12 -0400

    Walk the scene hierarchy to correctly handle child reference frames in the model

--- utilities/assimp_mesh_loader.hpp
+++ utilities/assimp_mesh_loader.hpp
@@ -9,7 +9,7 @@
 class AssimpMeshLoader {
 public:
 	AssimpMeshLoader(const std::string &pFile) : error(false) {
-		scene = importer.ReadFile(pFile, aiProcess_FixInfacingNormals | aiProcess_GenNormals);
+		scene = importer.ReadFile(pFile, aiProcess_FixInfacingNormals | aiProcess_GenNormals |  aiProcess_Triangulate);
 
 		if(!scene) {
 			fprintf(stderr, "%s\n", importer.GetErrorString());
@@ -21,9 +21,22 @@ public:
 				std::vector< std::vector<fcl::Triangle> > &triangles,
 				std::vector< std::vector<double> > &normals) const {
 
-		for(unsigned int i = 0; i < scene->mNumMeshes; ++i) {
+		aiMatrix4x4 identity;
+		walkSceneHierarchy(scene, scene->mRootNode, identity, vertices, triangles, normals);
+	}
 
-			auto mesh = scene->mMeshes[i];
+	void walkSceneHierarchy(const aiScene *scene, const aiNode *current, const aiMatrix4x4 &parentTransform,
+				std::vector< std::vector<fcl::Vec3f> > &vertices,
+				std::vector< std::vector<fcl::Triangle> > &triangles,
+				std::vector< std::vector<double> > &normals) const {
+
+		const aiMatrix4x4 &transform = parentTransform * current->mTransformation;
+
+		for(unsigned int j = 0; j < current->mNumMeshes; ++j) {
+			assert(current->mMeshes[j] < scene->mNumMeshes);
+			unsigned int meshIndex = current->mMeshes[j];
+
+			auto mesh = scene->mMeshes[meshIndex];
 
 			vertices.emplace_back();
 			triangles.emplace_back();
@@ -33,13 +46,19 @@ public:
 				double x = vertex.x;
 				double y = vertex.y;
 				double z = vertex.z;
-				vertices.back().emplace_back(x, y, z);
+
+				aiVector3D vec(x, y, z);
+				vec *= transform;
+
+				vertices.back().emplace_back(vec.x, vec.y, vec.z);
 
 				normals.emplace_back(3);
 				if(mesh->HasNormals()) {
-					normals.back()[0] = mesh->mNormals[j][0];
-					normals.back()[1] = mesh->mNormals[j][1];
-					normals.back()[2] = mesh->mNormals[j][2];
+					aiVector3D norm(mesh->mNormals[j][0], mesh->mNormals[j][1], mesh->mNormals[j][2]);
+					norm *= transform;
+					normals.back()[0] = norm.x;
+					normals.back()[1] = norm.y;
+					normals.back()[2] = norm.z;
 				} else {
 					normals.back()[0] = 1;
 					normals.back()[1] = 0;
@@ -59,6 +78,10 @@ public:
 												face.mIndices[1],
 												face.mIndices[2]);
 			}
+		}
+
+		for(unsigned int i = 0; i < current->mNumChildren; ++i) {
+			walkSceneHierarchy(scene, current->mChildren[i], transform, vertices, triangles, normals);
 		}
 	}
 
